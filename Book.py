2d3dfd12b9ab24460892f9aacfa7efad6c4084fe
boojo <jshim27@wisc.edu>
2022-06-28 17:18:08 -0500

    inital start to book class

--- Book.py
+++ Book.py
@@ -1,3 +1,23 @@
-import sys
+class Book():
+    def __init__(self, title, author, genre, rating):        
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.rating = rating
+    
+    def get_title(self):
+        return self.title
+        
+    def get_author(self):
+        return self.author
+    
+    def get_genre(self):
+        return self.genre
 
-print("Hello World")
+    def get_rating(self):
+        return self.rating
+    
+    def set_rating(self, rating=None):
+        self.rating = rating
+    
+
